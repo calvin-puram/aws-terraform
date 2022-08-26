7aa4d56a06e6d1b22991f03e07ba437aadb4ef0e
calvin <puram.calvin@gmail.com>
2022-08-26 10:57:45 +0100

    add s3 static confiqs

---
+++ s3-static-web/examples/main.tf
@@ -0,0 +1,4 @@
+module prod_website {
+  source = "github.com/calvin-puram/terraform/s3_website"
+  bucket_prefix = "test-bucket"
+}
---
+++ s3-static-web/variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_prefix" {
+  type        = string
+  default = "2ko-test-cloud"
+  description = "Name of the s3 bucket to be created."
+}
+
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "Name of the s3 bucket to be created."
+}
