daa323726fd28a1d002eb2884125dbcad182b92f
calvin <puram.calvin@gmail.com>
2022-08-26 11:24:01 +0100

    few changes on module

--- s3-static-web/examples/main.tf
+++ s3-static-web/examples/main.tf
@@ -1,4 +1,4 @@
 module prod_website {
-  source = "github.com/calvin-puram/terraform/s3_website"
+  source = "github.com/calvin-puram/aws-terraform/s3-static-web"
   bucket_prefix = "test-bucket"
 }
